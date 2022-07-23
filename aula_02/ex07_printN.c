a4c651e924ca89f62d86396ab96f76df2a2010d1
Marcos A. P. de Lima <marcos.lima@icomp.ufam.edu.br>
2022-07-23 11:29:22 -0400

    Update ex07_printN.c

--- aula_02/ex07_printN.c
+++ aula_02/ex07_printN.c
@@ -1,3 +1,10 @@
+/*
+	@author marcosmapl
+	@email marcos.lima@icomp.ufam.edu.br
+	@company O Cara dos Algoritmos
+	@since Oct 20, 2019
+	Exemplos de uso da função print com parametro %n.
+*/
 #include <stdio.h>
 
 int main()
