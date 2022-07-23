5c0827578b87a366e77db356b674cebe5fefa6c6
Marcos A. P. de Lima <marcos.lima@icomp.ufam.edu.br>
2022-07-23 11:26:12 -0400

    Update ex01_olamundo.c

--- aula_01/ex01_olamundo.c
+++ aula_01/ex01_olamundo.c
@@ -1,7 +1,7 @@
 /*
 	@author marcosmapl
 	@email marcos.lima@icomp.ufam.edu.br
-	@company Devcated
+	@company O Cara dos Algoritmos
 	@since Oct 20, 2019
 
 	Exemplo de um primeiro programa em linguagem c.
@@ -14,6 +14,6 @@ int main()
 	printf("Bem vindo ao Curso!");
 	printf("Do Zero Absoluto: Programação em C");
 	printf("Exemplo 01 - Olá Mundo!");
-	printf("Canal Devcated - Youtube - 2019");
+	printf("O Cara dos Algoritmos - Youtube");
 	return 0;
 }
